38d5f1e1fb3b2ecebbc37e34c024c17196954b21
pierredu659 <pierre659@outlook.fr>
2021-03-29 11:43:20 +0200

    29/03/2021

--- Pierre/src/test.c
+++ Pierre/src/test.c
@@ -3,6 +3,8 @@
 #include <SDL.h>
 #include "constantes.h"
 
+//gcc src/test.c -o bin/prog -I include -L lib -lmingw32 -lSDL2main -lSDL2 -mwindows
+
 void SDL_ExitWithError(const char *message);
 SDL_Rect position, positionJoueur;
 
@@ -13,7 +15,7 @@ int main(int argc, char *argv[])
     SDL_Surface *image = NULL;
     SDL_Texture *texture = NULL;
 
-
+int toutsepassebien=1;
 
 
     //Lancement SDL
@@ -21,35 +23,101 @@ int main(int argc, char *argv[])
         SDL_ExitWithError("Initialisation SDL");
 
     //Création fenêtre + rendu
-    if(SDL_CreateWindowAndRenderer(900, 700, 0, &window, &renderer) != 0)
+    if(SDL_CreateWindowAndRenderer(800, 450, 0, &window, &renderer) != 0)
         SDL_ExitWithError("Impossible de creer la fenetre et le rendu");
 
-    
-    /*
-    if(SDL_SetRenderDrawColor(renderer, 112, 168, 237, SDL_ALPHA_OPAQUE) != 0)
-        SDL_ExitWithError("Impossible de changer la couleur");
-
-    if(SDL_RenderDrawLine(renderer, 50, 50, 500, 500) != 0)
-        SDL_ExitWithError("Impossible de dessiner une ligne");
-
+    // niveau 1
+    if(toutsepassebien){
     SDL_Rect rectangle;
-    rectangle.x = 300;
-    rectangle.y = 300;
-    rectangle.w = 200;
-    rectangle.h = 100;
-
-    if(SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE) != 0)
+    // départ / arrivé
+    rectangle.x = 25;
+    rectangle.y = 25;
+    rectangle.w = 50;
+    rectangle.h = 50;
+    if(SDL_SetRenderDrawColor(renderer, 00, 80, 00, SDL_ALPHA_OPAQUE) != 0)
         SDL_ExitWithError("Impossible de changer la couleur");
-
-    if(SDL_RenderDrawRect(renderer, &rectangle) != 0)      //SDL_RenderFillRect dessine un rectangle rempli
+    if(SDL_RenderFillRect(renderer, &rectangle) != 0)      //SDL_RenderFillRect dessine un rectangle rempli / SDL_RenderDrawRect fais juste les contours
         SDL_ExitWithError("Impossible de dessiner un rectangle");
 
+    rectangle.x = 25;
+    rectangle.y = 375;
+    rectangle.w = 50;
+    rectangle.h = 50;
+    if(SDL_SetRenderDrawColor(renderer, 255, 00, 00, SDL_ALPHA_OPAQUE) != 0)
+        SDL_ExitWithError("Impossible de changer la couleur");
+    if(SDL_RenderFillRect(renderer, &rectangle) != 0)      //SDL_RenderFillRect dessine un rectangle rempli / SDL_RenderDrawRect fais juste les contours
+        SDL_ExitWithError("Impossible de dessiner un rectangle");
+SDL_RenderPresent(renderer);
+
+
+
+
+    //murs
+    rectangle.x = 100;
+    rectangle.y = 0;
+    rectangle.w = 50;
+    rectangle.h = 200;
+    if(SDL_SetRenderDrawColor(renderer, 80, 80, 80, SDL_ALPHA_OPAQUE) != 0)
+        SDL_ExitWithError("Impossible de changer la couleur");
+    if(SDL_RenderFillRect(renderer, &rectangle) != 0)      //SDL_RenderFillRect dessine un rectangle rempli / SDL_RenderDrawRect fais juste les contours
+        SDL_ExitWithError("Impossible de dessiner un rectangle");
+SDL_RenderPresent(renderer);
+
+    rectangle.x = 0;
+    rectangle.y = 300;
+    rectangle.w = 500;
+    rectangle.h = 50;
+    if(SDL_SetRenderDrawColor(renderer, 80, 80, 80, SDL_ALPHA_OPAQUE) != 0)
+        SDL_ExitWithError("Impossible de changer la couleur");
+    if(SDL_RenderFillRect(renderer, &rectangle) != 0)      //SDL_RenderFillRect dessine un rectangle rempli / SDL_RenderDrawRect fais juste les contours
+        SDL_ExitWithError("Impossible de dessiner un rectangle");
+SDL_RenderPresent(renderer);
+
+    rectangle.x = 600;
+    rectangle.y = 300;
+    rectangle.w = 250;
+    rectangle.h = 50;
+    if(SDL_SetRenderDrawColor(renderer, 80, 80, 80, SDL_ALPHA_OPAQUE) != 0)
+        SDL_ExitWithError("Impossible de changer la couleur");
+    if(SDL_RenderFillRect(renderer, &rectangle) != 0)      //SDL_RenderFillRect dessine un rectangle rempli / SDL_RenderDrawRect fais juste les contours
+        SDL_ExitWithError("Impossible de dessiner un rectangle");
+SDL_RenderPresent(renderer);
+
+    rectangle.x = 450;
+    rectangle.y = 100;
+    rectangle.w = 50;
+    rectangle.h = 200;
+    if(SDL_SetRenderDrawColor(renderer, 80, 80, 80, SDL_ALPHA_OPAQUE) != 0)
+        SDL_ExitWithError("Impossible de changer la couleur");
+    if(SDL_RenderFillRect(renderer, &rectangle) != 0)      //SDL_RenderFillRect dessine un rectangle rempli / SDL_RenderDrawRect fais juste les contours
+        SDL_ExitWithError("Impossible de dessiner un rectangle");
+SDL_RenderPresent(renderer);
+
+// miroirs
+    if(SDL_SetRenderDrawColor(renderer, 255, 192, 203, SDL_ALPHA_OPAQUE) != 0)
+        SDL_ExitWithError("Impossible de changer la couleur");
+    if(SDL_RenderDrawLine(renderer, 25, 225, 75, 275) != 0)
+        SDL_ExitWithError("Impossible de dessiner une ligne");
+    if(SDL_RenderDrawLine(renderer, 425, 225, 375, 275) != 0)
+        SDL_ExitWithError("Impossible de dessiner une ligne");
+    if(SDL_RenderDrawLine(renderer, 425, 25, 375, 75) != 0)
+        SDL_ExitWithError("Impossible de dessiner une ligne");
+    if(SDL_RenderDrawLine(renderer, 525, 25, 575, 75) != 0)
+        SDL_ExitWithError("Impossible de dessiner une ligne");
+    if(SDL_RenderDrawLine(renderer, 575, 375, 525, 425) != 0)
+        SDL_ExitWithError("Impossible de dessiner une ligne");
     SDL_RenderPresent(renderer);
-*/
+    }
+    
+
+
+
+    
 
 
 
 
+/*
     SDL_Surface *robot[4]={NULL};
     SDL_Surface *robotactuel=NULL;
 
@@ -68,7 +136,7 @@ int main(int argc, char *argv[])
     position.y=positionJoueur.y*TAILLE_BLOC;
 
 
-
+*/
 
 
 
