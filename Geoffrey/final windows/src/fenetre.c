415e63e8e564218fb5578ced87475017837664b4
bAAnaneDivine <mael.vassenet@gmail.com>
2021-04-18 17:56:04 +0200

    18/04/2021

--- Geoffrey/final windows/src/fenetre.c
+++ Geoffrey/final windows/src/fenetre.c
@@ -18,6 +18,8 @@ void fenetre(SDL_Window* window, SDL_Renderer* renderer)
     SDL_Surface* icon = NULL;
     SDL_Texture* texture = NULL;
 
+    int toutsepassebien = 1;
+
     
 
 
@@ -51,6 +53,88 @@ void fenetre(SDL_Window* window, SDL_Renderer* renderer)
 
     if (SDL_RenderFillRect(renderer, &rectangle) != 0)
         SDL_ExitWithError("Impossible de dessiner un rectangle");
+
+    /*if (toutsepassebien) {
+        SDL_Rect rectangle;
+        // départ / arrivé
+        rectangle.x = 25;
+        rectangle.y = 25;
+        rectangle.w = 50;
+        rectangle.h = 50;
+        if (SDL_SetRenderDrawColor(renderer, 00, 80, 00, SDL_ALPHA_OPAQUE) != 0)
+            SDL_ExitWithError("Impossible de changer la couleur");
+        if (SDL_RenderFillRect(renderer, &rectangle) != 0)      //SDL_RenderFillRect dessine un rectangle rempli / SDL_RenderDrawRect fais juste les contours
+            SDL_ExitWithError("Impossible de dessiner un rectangle");
+
+        rectangle.x = 25;
+        rectangle.y = 375;
+        rectangle.w = 50;
+        rectangle.h = 50;
+        if (SDL_SetRenderDrawColor(renderer, 255, 00, 00, SDL_ALPHA_OPAQUE) != 0)
+            SDL_ExitWithError("Impossible de changer la couleur");
+        if (SDL_RenderFillRect(renderer, &rectangle) != 0)      //SDL_RenderFillRect dessine un rectangle rempli / SDL_RenderDrawRect fais juste les contours
+            SDL_ExitWithError("Impossible de dessiner un rectangle");
+        SDL_RenderPresent(renderer);
+
+
+
+
+        //murs
+        rectangle.x = 100;
+        rectangle.y = 0;
+        rectangle.w = 50;
+        rectangle.h = 200;
+        if (SDL_SetRenderDrawColor(renderer, 80, 80, 80, SDL_ALPHA_OPAQUE) != 0)
+            SDL_ExitWithError("Impossible de changer la couleur");
+        if (SDL_RenderFillRect(renderer, &rectangle) != 0)      //SDL_RenderFillRect dessine un rectangle rempli / SDL_RenderDrawRect fais juste les contours
+            SDL_ExitWithError("Impossible de dessiner un rectangle");
+        SDL_RenderPresent(renderer);
+
+        rectangle.x = 0;
+        rectangle.y = 300;
+        rectangle.w = 500;
+        rectangle.h = 50;
+        if (SDL_SetRenderDrawColor(renderer, 80, 80, 80, SDL_ALPHA_OPAQUE) != 0)
+            SDL_ExitWithError("Impossible de changer la couleur");
+        if (SDL_RenderFillRect(renderer, &rectangle) != 0)      //SDL_RenderFillRect dessine un rectangle rempli / SDL_RenderDrawRect fais juste les contours
+            SDL_ExitWithError("Impossible de dessiner un rectangle");
+        SDL_RenderPresent(renderer);
+
+        rectangle.x = 600;
+        rectangle.y = 300;
+        rectangle.w = 250;
+        rectangle.h = 50;
+        if (SDL_SetRenderDrawColor(renderer, 80, 80, 80, SDL_ALPHA_OPAQUE) != 0)
+            SDL_ExitWithError("Impossible de changer la couleur");
+        if (SDL_RenderFillRect(renderer, &rectangle) != 0)      //SDL_RenderFillRect dessine un rectangle rempli / SDL_RenderDrawRect fais juste les contours
+            SDL_ExitWithError("Impossible de dessiner un rectangle");
+        SDL_RenderPresent(renderer);
+
+        rectangle.x = 450;
+        rectangle.y = 100;
+        rectangle.w = 50;
+        rectangle.h = 200;
+        if (SDL_SetRenderDrawColor(renderer, 80, 80, 80, SDL_ALPHA_OPAQUE) != 0)
+            SDL_ExitWithError("Impossible de changer la couleur");
+        if (SDL_RenderFillRect(renderer, &rectangle) != 0)      //SDL_RenderFillRect dessine un rectangle rempli / SDL_RenderDrawRect fais juste les contours
+            SDL_ExitWithError("Impossible de dessiner un rectangle");
+        SDL_RenderPresent(renderer);
+
+        // miroirs
+        if (SDL_SetRenderDrawColor(renderer, 255, 192, 203, SDL_ALPHA_OPAQUE) != 0)
+            SDL_ExitWithError("Impossible de changer la couleur");
+        if (SDL_RenderDrawLine(renderer, 25, 225, 75, 275) != 0)
+            SDL_ExitWithError("Impossible de dessiner une ligne");
+        if (SDL_RenderDrawLine(renderer, 425, 225, 375, 275) != 0)
+            SDL_ExitWithError("Impossible de dessiner une ligne");
+        if (SDL_RenderDrawLine(renderer, 425, 25, 375, 75) != 0)
+            SDL_ExitWithError("Impossible de dessiner une ligne");
+        if (SDL_RenderDrawLine(renderer, 525, 25, 575, 75) != 0)
+            SDL_ExitWithError("Impossible de dessiner une ligne");
+        if (SDL_RenderDrawLine(renderer, 575, 375, 525, 425) != 0)
+            SDL_ExitWithError("Impossible de dessiner une ligne");
+        SDL_RenderPresent(renderer);
+    }*/
 
 
 }
--- Geoffrey/final windows/src/main.c
+++ Geoffrey/final windows/src/main.c
@@ -13,11 +13,54 @@
 //gcc src/main.c -o bin/main -I include -L lib -lmingw32 -lSDL2main -lSDL2 -mwindows
 
 
-void action(SDL_Window* window, SDL_Renderer* renderer){
+
+int main(int argc, char* argv[])
+{
+    int const height = 800;
+    int const width = 450;
+
+    SDL_Window* window = NULL;
+    SDL_Renderer* renderer = NULL;
 
     SDL_Rect rectangle_pers;
     rectangle_pers.x = 1;
     rectangle_pers.y = 1;
+
+    SDL_Rect rectangle_restart;
+    rectangle_restart.x = (height / 2 / 1.5);
+    rectangle_restart.y = (width / 3 * 2);
+    rectangle_restart.w = 100;
+    rectangle_restart.h = 50;
+
+    SDL_Rect rectangle_quit;
+    rectangle_quit.x = (height / 2 * 1);
+    rectangle_quit.y = (width / 3 * 2);
+    rectangle_quit.w = 100;
+    rectangle_quit.h = 50;
+
+    int x = 0, y = 0;
+
+
+    //Lancement SDL
+    SDL_initialisation();
+
+    window = SDL_CreateWindow("[EN COURS] DE REP@RATION", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, height, width, 0);
+
+    if (window == NULL)
+        SDL_ExitWithError("Impossible de creer la fen�tre");
+
+
+
+
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    if (renderer == NULL) {
+        SDL_DestroyWindow(window);
+        SDL_ExitWithError("Impossible de creer le rendu");
+    }
+
+    menu(window, renderer, rectangle_restart, rectangle_quit);
+    SDL_RenderPresent(renderer);
 
     SDL_bool program_launched = SDL_TRUE;
 
@@ -32,11 +75,42 @@ void action(SDL_Window* window, SDL_Renderer* renderer){
                 program_launched = SDL_FALSE;
                 break;
 
-            case SDL_KEYDOWN:
-                deplacer(event, &rectangle_pers);
-                fenetre(window, renderer);
-                afficher_personnage(window, renderer, rectangle_pers);
-                SDL_RenderPresent(renderer);
+            case SDL_MOUSEBUTTONDOWN: // Relâchement d'un clique
+                x = event.button.x;  //récupération de la position de la souris
+                y = event.button.y;
+                while ((x > rectangle_restart.x && x<(rectangle_restart.x + rectangle_restart.w) && y>rectangle_restart.y && y < (rectangle_restart.y + rectangle_restart.h)) && program_launched)
+                {
+
+                    fenetre(window, renderer);
+                    afficher_personnage(window, renderer, rectangle_pers);
+                    SDL_RenderPresent(renderer);
+
+
+                    while (SDL_PollEvent(&event))
+                    {
+                        switch (event.type)
+                        {
+                        case SDL_KEYDOWN:
+                            deplacer(event, &rectangle_pers);
+                            fenetre(window, renderer);
+                            afficher_personnage(window, renderer, rectangle_pers);
+                            SDL_RenderPresent(renderer);
+                            break;
+
+                        case SDL_QUIT:
+                            program_launched = SDL_FALSE;
+                            break;
+
+                        default:
+                            break;
+                        }
+                    }
+                    //////
+                }
+                if (x > rectangle_quit.x && x<(rectangle_quit.x + rectangle_quit.w) && y>rectangle_quit.y && y < (rectangle_quit.y + rectangle_quit.h))
+                {
+                    program_launched = SDL_FALSE;
+                }
                 break;
 
             default:
@@ -44,45 +118,7 @@ void action(SDL_Window* window, SDL_Renderer* renderer){
             }
         }
     }
-}
 
-int main(int argc, char* argv[])
-{
-
-    SDL_Window* window = NULL;
-    SDL_Renderer* renderer = NULL;
-
-
-
-    //Lancement SDL
-    SDL_initialisation();
-
-    int height = 800;
-    int width = 450;
-    
-
-    window = SDL_CreateWindow("[EN COURS] DE REP@RATION", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, height, width, 0);
-
-    if (window == NULL)
-        SDL_ExitWithError("Impossible de creer la fen�tre");
-
-    
-
-
-    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-
-    if (renderer == NULL) {
-        SDL_DestroyWindow(window);
-        SDL_ExitWithError("Impossible de creer le rendu");
-    }
-
-    menu(window, renderer);
-
-    //fenetre(window, renderer);
-    
-    //afficher_personnage(window, renderer, rectangle_pers);
-
-    SDL_RenderPresent(renderer);
 
 
     SDL_DestroyRenderer(renderer);
